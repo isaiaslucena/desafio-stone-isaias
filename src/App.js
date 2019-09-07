7811e209cfbc5714b6afeee6903002efb53f2ce3
Isaias Neto <isaiasneto@gmail.com>
2019-09-07 13:47:43 -0300

    Added routes with test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <Router>
+      <div>
+        <Header />
+
+        <Route exact path="/" component={DefaultHome} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  );
+}
+
+function DefaultHome() {
+  return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +35,31 @@ function App() {
         </a>
       </header>
     </div>
+  )
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/topics">Topics</Link>
+      </li>
+    </ul>
   );
+}
+
+function About() {
+  return <h3>About Test Route</h3>
+}
+
+function Topics() {
+  return <h3>Topics Test Route</h3>
 }
 
 export default App;
