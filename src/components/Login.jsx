e111f86b7557ca751be7b82ee7556614635d9270
Isaias Neto <isaiasneto@gmail.com>
2019-09-07 21:39:57 -0300

    Remove indentation for some elements

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,57 +27,28 @@ class Login extends React.Component {
                         Sign in
                     </Typography>
                     <form className="form" noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className="submit"
-                    >
+                    <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus/>
+                    <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password"/>
+                    <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+                    <Button type="submit" fullWidth variant="contained" color="primary" className="submit">
                         Sign In
                     </Button>
                     <Grid container>
                         <Grid item xs>
-                        <Link href="#" variant="body2">
-                            Forgot password?
-                        </Link>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
                         </Grid>
                         <Grid item>
-                        <Link href="#" variant="body2">
-                            {"Don't have an account? Sign Up"}
-                        </Link>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
                         </Grid>
                     </Grid>
                     </form>
                 </div>
                 <Box mt={8}>
-                    Copyright
+                    {"new Date().toLocaleDateString();"}
                 </Box>
             </Container>
         )
