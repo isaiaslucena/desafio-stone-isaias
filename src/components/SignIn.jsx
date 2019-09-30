736016a2ec1d67bd716fa27f532d870aa4a50f88
Isaias Neto <isaiasneto@gmail.com>
2019-09-29 23:50:54 -0300

    Commented unused lines

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -28,17 +28,17 @@ class Login extends React.Component {
 	GoogleLoginClick() {
 		firebase.auth().signInWithPopup(provider).then(function(result) {
 			// This gives you a Google Access Token. You can use it to access the Google API.
-			var token = result.credential.accessToken;
+			// var token = result.credential.accessToken;
 			// The signed-in user info.
-			var user = result.user;
+			// var user = result.user;
 		}).catch(function(error) {
 			// Handle Errors here.
-			var errorCode = error.code;
-			var errorMessage = error.message;
+			// var errorCode = error.code;
+			// var errorMessage = error.message;
 			// The email of the user's account used.
-			var email = error.email;
+			// var email = error.email;
 			// The firebase.auth.AuthCredential type that was used.
-			var credential = error.credential;
+			// var credential = error.credential;
 		});
 	}
 
